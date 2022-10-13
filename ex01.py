159d2a104f40c14fd3d5c4c026216821fe66219f
Ekaterina Anikina <reityan97@mail.ru>
2022-10-13 23:46:45 +0300

    Поправлен способ округления

--- ex01.py
+++ ex01.py
@@ -3,7 +3,7 @@
 from unittest import result
 
 num = input('Введите число d: ')
-d = len(num) - num.find('.')
+d = len(num) - num.find('.')-1
 result = sum(1/16**x*(4/(8*x + 1) - 2/(8*x + 4) - 1/(8*x + 5) - 1/(8*x + 6)) for x in range(d))
 
-print(str(result)[:len(num)])
+print(str(round(result, d)))
