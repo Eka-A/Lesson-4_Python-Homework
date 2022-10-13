b64c07d0ab0ff8ddef69c0d90fbd5f448085eb73
Ekaterina Anikina <reityan97@mail.ru>
2022-10-13 23:34:18 +0300

    Редакция строк

--- ex01.py
+++ ex01.py
@@ -2,7 +2,6 @@
 
 from unittest import result
 
-
 num = input('Введите число d: ')
 d = len(num) - num.find('.')
 result = sum(1/16**x*(4/(8*x + 1) - 2/(8*x + 4) - 1/(8*x + 5) - 1/(8*x + 6)) for x in range(d))
